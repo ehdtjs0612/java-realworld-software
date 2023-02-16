d772736e0e8ac2c3692e25e79b02050606a2c0b8
YooDongSeon <inko51366@naver.com>
2023-02-16 16:32:05 +0900

    feat: 데이터를 파싱하는 기능을 추상화

--- src/main/java/BankStatementParser.java
+++ src/main/java/BankStatementParser.java
@@ -1,4 +1,10 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface BankStatementParser {
 
     // CSV, JSON, XML 등 다른 파일을 읽어올수있는 메서드들 정의
+    BankTransaction parse(String line);
+    public List<BankTransaction> parseLine(List<String> lines);
 }
