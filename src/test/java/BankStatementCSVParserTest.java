413029b36b54c8c6e771962451713dda4ff1dfac
YooDongSeon <inko51366@naver.com>
2023-02-17 09:57:35 +0900

    test: CSVParser의 parseLine메서드 테스트 추가

--- src/test/java/BankStatementCSVParserTest.java
+++ src/test/java/BankStatementCSVParserTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +25,23 @@ class BankStatementCSVParserTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldParse() {
+        // given
+        List<String> list = new ArrayList<>();
+        list.add("30-01-2017,-100,Deliveroo");
+        list.add("30-01-2017,-50,Tesco");
+        list.add("01-02-2017,6000,Salary");
+
+        // when
+        List<BankTransaction> bankTransactions = statementParser.parseLine(list);
+
+
+        // then
+        for(BankTransaction bankTransaction : bankTransactions) {
+            System.out.println(bankTransaction);
+        }
     }
 }
